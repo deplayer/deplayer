e68414588d0f2c57120430632e06234c507d2077
Genar Trias Ortiz <genar@acs.li>
2018-05-08 20:55:50 +0200

    Fixing tests

---
+++ src/reducers/collection.spec.js
@@ -0,0 +1,12 @@
+import reducer from './collection'
+
+describe('collection reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {}))
+      .toEqual(
+        {"rows": [], "totalRows": 0, "visibleSongs": [], "visibleSongsIds": []}
+      )
+  })
+})
+
+
