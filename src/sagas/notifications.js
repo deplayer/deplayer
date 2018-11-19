43e760c9fb8964f3b1f65efa8589d49d76f31ee9
Genar Trias Ortiz <genar@acs.li>
2018-11-19 22:30:15 +0100

    First notification shown to user, refs #25

--- src/sagas/notifications.js
+++ src/sagas/notifications.js
@@ -8,12 +8,18 @@ import * as types from '../constants/ActionTypes'
 // Handling START_PLAYING saga
 export function* sendCurrentPlayingNotification(action: any): any {
   const notificationService = new NotificatoinService()
-  yield call(notificationService.initialize)
+  yield call(notificationService.sendNotification)
+}
+
+export function* setupNotifications(): any {
+  const notificationService = new NotificatoinService()
+  yield call(notificationService.requestPermission)
 }
 
 // Binding actions to sagas
 function* notificationSaga(): Generator<void, void, void> {
   yield takeLatest(types.SET_CURRENT_PLAYING, sendCurrentPlayingNotification)
+  yield takeLatest(types.INITIALIZED, setupNotifications)
 }
 
 export default notificationSaga
--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -1,18 +1,34 @@
 // @flow
 
 export default class NotificationService {
-  constructor() {
+  sendNotification = () => {
+    const Notification = this.getNotificationClass()
+    if (Notification) {
+      new Notification("Welcome to genar-radio!");
+    }
+  }
+
+  getNotificationClass () {
     const Notification = window.Notification
 
     // Let's check if the browser supports notifications
     if (!Notification) {
-      alert("This browser does not support desktop notification");
+      console.log("This browser does not support desktop notification");
     }
 
     // Let's check whether notification permissions have already been granted
     else if (Notification.permission === "granted") {
       // If it's okay let's create a notification
       new Notification("Welcome to genar-radio!");
+    }
+
+    return Notification
+  }
+
+  requestPermission = () => {
+    const Notification = this.getNotificationClass()
+    if (Notification) {
+      Notification.requestPermission()
     }
   }
 }
