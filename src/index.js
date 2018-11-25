f63906043fbc78f22c8544756252269d870009bc
Genar Trias Ortiz <genar@acs.li>
2018-11-26 00:43:48 +0100

    Trying to hide browser bar on mobile

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// Browser bar hack
+window.scrollTo(0,1)
 
 registerServiceWorker();
