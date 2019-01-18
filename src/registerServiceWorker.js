c8175418ea1f15e9a224616838f85f93260be080
Genar Trias Ortiz <genar@acs.li>
2019-01-18 01:22:23 +0100

    Removing serviceworker addEventListener

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -115,16 +115,3 @@ export function unregister() {
     });
   }
 }
-
-window.self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-})
