3db9cf2e108ecb2ef1b45bf399982df62209ef26
Genar Trias Ortiz <genar@acs.li>
2019-01-22 09:47:12 +0100

    Start to migrate to typescript

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -82,7 +82,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     // `web` extension prefixes have been added for better support
     // for React Native Web.
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx', '.ts', '.tsx'],
     alias: {
 
       // Support React Native Web
@@ -120,6 +120,11 @@ module.exports = {
             loader: require.resolve('eslint-loader'),
           },
         ],
+        include: paths.appSrc,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
         include: paths.appSrc,
       },
       {
