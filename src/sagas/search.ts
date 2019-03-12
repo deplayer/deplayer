d4e9660ce630fdc5b0323cb7ae8299b08c4e65e9
Genar Trias Ortiz <genar@acs.li>
2019-03-12 19:36:38 +0100

    stop enqueuing search results

--- src/sagas/search.ts
+++ src/sagas/search.ts
@@ -27,7 +27,6 @@ export function* search(action: SearchAction): any {
     for (const result in searchResults) {
       yield put({type: types.SEARCH_FULLFILLED, result: searchResults[result]})
       yield put({type: types.ADD_TO_COLLECTION, data: searchResults[result]})
-      yield put({type: types.ADD_SONGS_TO_QUEUE, songs: searchResults[result]})
     }
   } catch (e) {
     yield put({type: types.SEARCH_REJECTED, message: e.message})
