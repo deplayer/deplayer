6ffa30a9f476d68ef0bbf461aa51ba6082171962
Genar Trias Ortiz <genar@acs.li>
2019-06-07 20:19:04 +0200

    split ProviderForm

---
+++ src/components/Settings/ProviderForm.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { Dispatch } from 'redux'
+import { State as SettingsStateType } from '../../reducers/settings'
+import * as types from '../../constants/ActionTypes'
+import FormSchema from './FormSchema'
+
+type ProviderFormProps = {
+  providerKey: string,
+  settings: SettingsStateType,
+  dispatch: Dispatch
+}
+
+const ProviderForm = (props: ProviderFormProps) => {
+  const RemoveProviderBtn = (props: any) => {
+    const onClick = () => {
+      props.dispatch({type: types.REMOVE_PROVIDER, providerKey: props.providerKey})
+    }
+    return (
+      <div className='float-right'>
+        <a
+          className='btn btn-secondary'
+          onClick={onClick}
+          title={ props.providerKey }
+        >
+          <i className='fa fa-remove'></i>
+        </a>
+      </div>
+    )
+  }
+  return (
+    <div key={props.providerKey} className='card provider-card'>
+      <div className='card-body'>
+        <FormSchema schema={props.settings.settingsForm.providers[props.providerKey]} />
+      </div>
+
+      <div className='card-footer'>
+        <RemoveProviderBtn
+          providerKey={props.providerKey}
+          dispatch={props.dispatch}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ProviderForm
--- src/components/Settings/SettingsForm.tsx
+++ src/components/Settings/SettingsForm.tsx
@@ -7,50 +7,12 @@ import ProviderButton from './ProviderButton'
 import * as types from '../../constants/ActionTypes'
 import FormSchema from './FormSchema'
 import { State as SettingsStateType } from '../../reducers/settings'
+import ProviderForm from './ProviderForm'
 
 type Props = {
   settings: SettingsStateType,
   dispatch: Dispatch,
   schema: any
-}
-
-type ProviderFormProps = {
-  providerKey: string,
-  settings: SettingsStateType,
-  dispatch: Dispatch
-}
-
-const ProviderForm = (props: ProviderFormProps) => {
-  const RemoveProviderBtn = (props: any) => {
-    const onClick = () => {
-      props.dispatch({type: types.REMOVE_PROVIDER, providerKey: props.providerKey})
-    }
-    return (
-      <div className='float-right'>
-        <a
-          className='btn btn-secondary'
-          onClick={onClick}
-          title={ props.providerKey }
-        >
-          <i className='fa fa-remove'></i>
-        </a>
-      </div>
-    )
-  }
-  return (
-    <div key={props.providerKey} className='card provider-card'>
-      <div className='card-body'>
-        <FormSchema schema={props.settings.settingsForm.providers[props.providerKey]} />
-      </div>
-
-      <div className='card-footer'>
-        <RemoveProviderBtn
-          providerKey={props.providerKey}
-          dispatch={props.dispatch}
-        />
-      </div>
-    </div>
-  )
 }
 
 const SettingsForm = (props: Props) => {
