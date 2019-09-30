64a58a597f97c486b3584b61452313b99f36b7d4
Genar Trias Ortiz <genar@acs.li>
2019-10-01 01:15:15 +0200

    achieved non-blocking fetch of ipfs hashes

--- src/sagas/providers.ts
+++ src/sagas/providers.ts
@@ -1,13 +1,15 @@
-import { all, put, takeLatest, select } from 'redux-saga/effects'
+import { all, put, fork, take, takeLatest, select } from 'redux-saga/effects'
 import ipfsClient from 'ipfs-http-client'
 
 import  * as types from '../constants/ActionTypes'
+
+const mediaExp = new RegExp(".*\.(mp3|wav|avi)$")
 
 const getIpfsSettings = (state: any): any => {
   return state ? state.settings : {}
 }
 
-export function* startProvidersScan(): any {
+export function* scanFolder(hash: string) {
   const settings = yield select(getIpfsSettings)
   const ipfsSettings = settings.settings.providers['ipfs1']
   const ipfs = ipfsClient({
@@ -15,14 +17,50 @@ export function* startProvidersScan(): any {
     port: ipfsSettings.port,
     protocol: 'http'
   })
-  const files = yield ipfs.ls(ipfsSettings.hash)
+  try {
+    const files = yield ipfs.ls(hash)
+    yield put({ type: types.IPFS_FOLDER_SCANNED, files })
+  } catch {
+    yield put({ type: types.IPFS_FOLDER_SCAN_FAILED })
+  }
+}
+
+export function* startFolderScan(action: any): any {
+  yield fork(scanFolder, action.hash)
+  const { files } = yield take([types.IPFS_FOLDER_SCANNED])
+
   yield all(files.map((file: any) => {
-    return put({ type: types.LOAD_IPFS_FILE, file: file.path })
+    if (file.type === 'dir') {
+      return put({ type: types.START_IPFS_FOLDER_SCAN, hash: file })
+    }
+
+    if (file.path.match(mediaExp)) {
+      return put({ type: types.LOAD_IPFS_FILE, file: file.path })
+    }
+
+    return put({ type: types.IPFS_NON_SUPPORTED_ITEM, file: file.path })
+  }))
+}
+
+export function* startProvidersScan(): any {
+  const settings = yield select(getIpfsSettings)
+  const ipfsSettings = settings.settings.providers['ipfs1']
+
+  yield fork(scanFolder, ipfsSettings.hash)
+  const { files } = yield take([types.IPFS_FOLDER_SCANNED])
+
+  yield all(files.map((file: any) => {
+    if (file.path.match(mediaExp)) {
+      return put({ type: types.LOAD_IPFS_FILE, file: file.path })
+    } else {
+      return put({ type: types.START_IPFS_FOLDER_SCAN, hash: file })
+    }
   }))
 }
 
 // Binding actions to sagas
 function* providersSaga(): any {
+  yield takeLatest(types.START_IPFS_FOLDER_SCAN, startFolderScan)
   yield takeLatest(types.START_SCAN_SOURCES, startProvidersScan)
 }
 
