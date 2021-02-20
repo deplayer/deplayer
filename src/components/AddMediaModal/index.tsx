2a63ccd7061814b9264a826a6d4054fd8274a71f
Genar Trias Ortiz <genar@acs.li>
2021-02-20 02:24:40 +0100

    more UI niceties

--- src/components/AddMediaModal/index.tsx
+++ src/components/AddMediaModal/index.tsx
@@ -34,83 +34,92 @@ const AddMediaModal = (props: Props) => {
         <Header>
           Magnet link (powered by <a target="_blank" rel="noopener noreferrer" href='https://webtorrent.io/'><span>webtorrent</span></a>. check some <a target="_blank" rel="noopener noreferrer" href="https://webtorrent.io/free-torrents"><span>examples</span></a>)
         </Header>
-        <Input
-          type="text"
-          value={magnetLink}
-          onChange={(event) => setMagnetLink(event.target.value)}
-        />
+        <div className='my-2'>
+          <Input
+            type="text"
+            value={magnetLink}
+            onChange={(event) => setMagnetLink(event.target.value)}
+          />
+        </div>
 
         <input className='my-4' type="file" name="file" onChange={(event) => event.target.files && setTorrent(event.target.files[0])}/>
 
-        <Button
-          fullWidth
-          type='submit'
-          onClick={() => {
-            if (magnetLink !== '') {
-              props.dispatch({type: types.ADD_WEBTORRENT_MEDIA, magnet: magnetLink})
-            }
-            if (torrent) {
-              console.log('torrent: ', torrent)
-              // console.log('parsed torrent: ', parseTorrent.remote(torrent))
-              parseTorrent.remote(torrent, (err, parsedTorrent) => {
-                if (err) throw err
-                if (!parsedTorrent) return
+        <div className='flex justify-end'>
+          <Button
+            type='submit'
+            onClick={() => {
+              if (magnetLink !== '') {
+                props.dispatch({type: types.ADD_WEBTORRENT_MEDIA, magnet: magnetLink})
+              }
+              if (torrent) {
+                console.log('torrent: ', torrent)
+                // console.log('parsed torrent: ', parseTorrent.remote(torrent))
+                parseTorrent.remote(torrent, (err, parsedTorrent) => {
+                  if (err) throw err
+                  if (!parsedTorrent) return
 
-                const magnet = parseTorrent.toMagnetURI(parsedTorrent)
-                props.dispatch({type: types.ADD_WEBTORRENT_MEDIA, magnet: magnet})
-              })
-            }
-            props.dispatch({type: types.HIDE_ADD_MEDIA_MODAL})
-          }}
-        >
-          Add and fetch
-        </Button>
+                  const magnet = parseTorrent.toMagnetURI(parsedTorrent)
+                  props.dispatch({type: types.ADD_WEBTORRENT_MEDIA, magnet: magnet})
+                })
+              }
+              props.dispatch({type: types.HIDE_ADD_MEDIA_MODAL})
+            }}
+          >
+            Add and fetch
+          </Button>
+        </div>
       </div>
 
       <div className='my-6'>
         <Header>Youtube link</Header>
-        <Input
-          type="text"
-          value={youtubeLink}
-          onChange={(event) => setYoutubeLink(event.target.value)}
-        />
-        <Button
-          fullWidth
-          type='submit'
-          onClick={() => {
-            props.dispatch({
-              type: types.START_YOUTUBE_DL_SERVER_SCAN,
-              key: 'youtube',
-              data: { url: youtubeLink }
-            })
-            props.dispatch({type: types.HIDE_ADD_MEDIA_MODAL})
-          }}
-        >
-          Add and fetch
-        </Button>
+        <div className='my-2'>
+          <Input
+            type="text"
+            value={youtubeLink}
+            onChange={(event) => setYoutubeLink(event.target.value)}
+          />
+        </div>
+        <div className='flex justify-end'>
+          <Button
+            type='submit'
+            onClick={() => {
+              props.dispatch({
+                type: types.START_YOUTUBE_DL_SERVER_SCAN,
+                key: 'youtube',
+                data: { url: youtubeLink }
+              })
+              props.dispatch({type: types.HIDE_ADD_MEDIA_MODAL})
+            }}
+          >
+            Add and fetch
+          </Button>
+        </div>
       </div>
 
       <div className='my-6'>
         <Header>IPFS Hash</Header>
-        <Input
-          type="text"
-          value={ipfsHash}
-          onChange={(event) => setIpfsHash(event.target.value)}
-        />
-        <Button
-          fullWidth
-          type='submit'
-          onClick={() => {
-            props.dispatch({
-              type: types.SEND_NOTIFICATION,
-              notification: `starting to scan hash: ${ ipfsHash }`,
-              level: 'info'
-            })
-            props.dispatch({type: types.IPFS_FOLDER_FOUND, hash: ipfsHash })
-          }}
-        >
-          Add and fetch
-        </Button>
+        <div className='my-2'>
+          <Input
+            type="text"
+            value={ipfsHash}
+            onChange={(event) => setIpfsHash(event.target.value)}
+          />
+        </div>
+        <div className='flex justify-end'>
+          <Button
+            type='submit'
+            onClick={() => {
+              props.dispatch({
+                type: types.SEND_NOTIFICATION,
+                notification: `starting to scan hash: ${ ipfsHash }`,
+                level: 'info'
+              })
+              props.dispatch({type: types.IPFS_FOLDER_FOUND, hash: ipfsHash })
+            }}
+          >
+            Add and fetch
+          </Button>
+        </div>
       </div>
     </Modal>
   )
