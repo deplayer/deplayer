e273027e65e2d2e090779b5f5e37c13115a240fa
Genar Trias Ortiz <genar@acs.li>
2021-05-10 01:26:45 +0200

    disable strict mode

--- src/utils/format-utils.ts
+++ src/utils/format-utils.ts
@@ -1,4 +1,4 @@
-export const isTrack = (file) => {
+export const isTrack = (file: string) => {
   const results = file.match(/^.*.[wav|ogg|mp3]$/)
   if (results && results.length) {
     return true
