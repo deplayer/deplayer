c467c6bc189156758af37bc64b8231b81769677b
Genar Trias Ortiz <gtrias@gmail.com>
2024-06-22 22:40:05 +0200

    installing surge to allow deployments

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [],
   test: {
+    coverage: {
+      provider: 'v8'
+    },
     setupFiles: ['vitest-setup.ts'],
     environment: 'happy-dom'
   },
