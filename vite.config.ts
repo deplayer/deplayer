c070a37f768ac37af6a3df698e5ab46867d92943
Genar Trias Ortiz <gtrias@gmail.com>
2024-06-22 22:21:29 +0200

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import fixReactVirtualized from 'esbuild-plugin-react-virtualized'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
       plugins: [fixReactVirtualized],
+    },
+  },
+  resolve: {
+    alias: {
+      'webtorrent': fileURLToPath(new URL('./node_modules/webtorrent/dist/webtorrent.min.js', import.meta.url)),
     },
   },
 })
