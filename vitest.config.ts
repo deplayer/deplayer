d854efc4149fec1f152c555d43dd22588e31890d
Genar Trias Ortiz <gtrias@gmail.com>
2024-06-26 01:32:48 +0200

    Adding vite-pwa

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vitest/config'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto'
+    })
+  ],
   test: {
     coverage: {
       provider: 'v8'
