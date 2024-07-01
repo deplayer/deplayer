3cf4fc550981984861535fcf146a86592c8de225
Genar Trias Ortiz <gtrias@gmail.com>
2024-07-01 23:48:58 +0200

    fix tsc type error

--- src/containers/AlbumContainer.ts
+++ src/containers/AlbumContainer.ts
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import AlbumView from '../components/AlbumView'
 
 export default connect(
-  (state: any) => ({
+  (state: any, _ownProps: any) => ({
     songsByAlbum: state.collection.songsByAlbum,
     backgroundImage: state.app.backgroundImage,
     albums: state.collection.albums,
