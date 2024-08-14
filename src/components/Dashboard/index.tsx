3f7fe81b34045448b4714c73b22e5fb4c527690c
Genar Trias Ortiz <gtrias@gmail.com>
2024-08-15 01:20:56 +0200

    adding more images to show in home

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,14 +1,52 @@
 import { Translate } from 'react-redux-i18n'
+import React from 'react'
 
-import BodyMessage from '../BodyMessage'
 import MediaSlider from '../MediaSlider'
 import RelatedAlbums from '../RelatedAlbums'
-import { Link } from 'react-router-dom'
 import { State as CollectionState } from '../../reducers/collection'
+import AddNewMediaButton from '../Buttons/AddNewMediaButton'
+import { Link } from 'react-router-dom'
 
 type Props = {
   collection: CollectionState
 }
+
+const WelcomeMessage = () => {
+  const images = [
+    'record-player.svg',
+    'cassete.svg',
+    'walkman.svg',
+    'discman.svg',
+    'headset.svg'
+  ]
+  const pickImage = () => images[Math.floor(Math.random() * images.length)]
+  const randomImage = React.useMemo(pickImage, [])
+
+  return (
+    <div className='flex flex-col md:flex-row w-full content-start items-center md:items-start'>
+      <img
+        className='w-60'
+        src={randomImage}
+        alt={'Listen you good old music collection with deplayer'}
+
+      />
+      <div className='px-6 py-8'>
+        <h2 className='text-4xl'>Hi Stranger! Welcome to deplayer</h2>
+        <p className='py-4'>
+          Access to you good old music library and enjoy it whenever you need it. <br />
+          To start using deplayer add some music to your collection, either by
+          adding a provider (Subsonic the most tested one)
+        </p>
+        <ul>
+          <li><Link to='/providers' className='text-blue-500'>Setup your media providers</Link></li>
+          <li><AddNewMediaButton label="Add new media to your collection" /></li>
+          <li><Link to='/collection' className='text-blue-500'>Or go to your collection</Link></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 
 const Dashboard = ({ collection: { loading, rows, songsByNumberOfPlays, albums } }: Props) => {
   const MAX_LIST_ITEMS = 25
@@ -22,43 +60,16 @@ const Dashboard = ({ collection: { loading, rows, songsByNumberOfPlays, albums }
     .slice(0, MAX_LIST_ITEMS)
     .map((albumId) => albums[albumId])
 
-  if (!slicedAlbums.length && !mediaItems.length) {
-    return (
-      <BodyMessage message={
-        <div>
-          Welcome to Deplayer!
-          <br />
-          <Translate value='message.noMostPlayed' />
-          <br />
-          <Link to={'/collection'} className='h-32 button'><Translate value='message.goToCollection' /></Link>
-        </div>
-      } />
-    )
-  }
-
   return (
     <div className='z-10 w-full block px-12 mb-12'>
-      <div className='flex w-full content-start items-start'>
-        <img
-          className='w-60'
-          src={`/cassete.svg`}
-          alt={'Listen you good old music collection with deplayer'}
-
-        />
-        <div className='px-6 py-14'>
-          <h2 className='text-4xl'>Hi Stranger! Welcome to deplayer</h2>
-          <p className='py-4'>
-            Access to you good old music library and enjoy it whenever you need it. <br />
-          </p>
-        </div>
-      </div>
-
-      <MediaSlider
-        loading={loading}
-        title={<Translate value='titles.mostPlayedSongs' />}
-        mediaItems={mediaItems.slice(0, MAX_LIST_ITEMS)}
-      />
-      <RelatedAlbums albums={slicedAlbums} />
+      <WelcomeMessage></WelcomeMessage>
+      {!!mediaItems.length &&
+        <MediaSlider
+          loading={loading}
+          title={<Translate value='titles.mostPlayedSongs' />}
+          mediaItems={mediaItems.slice(0, MAX_LIST_ITEMS)}
+        />}
+      {!!slicedAlbums.length && <RelatedAlbums albums={slicedAlbums} />}
     </div>
   )
 }
