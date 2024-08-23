ced10b0664fb2abea8cd6e4e22d41fe3e85cda11
Genar Trias Ortiz <gtrias@gmail.com>
2024-08-23 09:54:20 +0200

    define custom bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'deplayer-dark': '#101b26',
+      },
+    },
   },
   plugins: [],
 }
