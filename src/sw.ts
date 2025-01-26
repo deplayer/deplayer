291c3baae6959ba42bfea2df7630a4563ec3fa23
Genar Trias Ortiz <gtrias@gmail.com>
2025-01-27 00:53:56 +0100

    recovering old sw.ts

--- src/sw.ts
+++ src/sw.ts
@@ -1,9 +1,5 @@
 import { clientsClaim } from "workbox-core";
 import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
-import { registerRoute } from "workbox-routing";
-import { CacheFirst, StaleWhileRevalidate } from "workbox-strategies";
-import { CacheableResponsePlugin } from "workbox-cacheable-response";
-import { ExpirationPlugin } from "workbox-expiration";
 import listener from "./worker-server";
 
 declare let self: ServiceWorkerGlobalScope;
@@ -16,54 +12,14 @@ clientsClaim();
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Cache CSS with StaleWhileRevalidate strategy
-registerRoute(
-  ({ request }) => request.destination === "style",
-  new StaleWhileRevalidate({
-    cacheName: "styles-cache",
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
-);
-
-// Cache images with CacheFirst strategy
-registerRoute(
-  ({ request }) => request.destination === "image",
-  new CacheFirst({
-    cacheName: "images-cache",
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 1000, // Maximum number of images to cache
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        purgeOnQuotaError: true, // Automatically cleanup if storage quota is exceeded
-      }),
-    ],
-  })
-);
-
 self.addEventListener("fetch", async (event) => {
   const url = new URL(event.request.url);
-
-  // Only handle requests to our own origin
-  if (url.origin === self.origin) {
-    if (url.pathname.startsWith("/opfs-")) {
-      event.respondWith(handleOpfsRequest(url.pathname));
-      return;
-    }
-
-    const res = listener(event);
-    if (res) {
-      event.respondWith(res);
-      return;
-    }
+  if (url.origin === self.origin && url.pathname.startsWith("/opfs-")) {
+    event.respondWith(handleOpfsRequest(url.pathname));
   }
-  // Let other requests (including CORS) pass through without interference
+
+  const res = listener(event);
+  if (res) event.respondWith(res);
 });
 
 async function handleOpfsRequest(pathname: string) {
