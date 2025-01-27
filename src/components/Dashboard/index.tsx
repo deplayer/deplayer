0965d7f9c621cc8b794c854038ce89412b589f23
Genar Trias Ortiz <gtrias@gmail.com>
2025-01-27 23:00:01 +0100

    fix issues with typography

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -21,8 +21,7 @@ import Auth from '../Auth'
 import Button from '../common/Button'
 import { Dispatch } from 'redux'
 import DeplayerTitle from '../DeplayerTitle'
-import { IMedia } from '../entities/media'
-
+import { IMedia } from '../../entities/Media'
 type Props = {
   collection: CollectionState
   dispatch: Dispatch
@@ -54,40 +53,42 @@ const WelcomeMessage = ({ dispatch, rows }: { dispatch: Dispatch, rows: Record<s
   const credentials = localStorage.getItem('credentials')
 
   return (
-    <div className='flex flex-col md:flex-row w-full content-start items-center'>
+    <div className='flex flex-col md:flex-row w-full content-start'>
       {showAuthModal && <Auth dispatch={dispatch} onClose={() => setShowAuthModal(false)} isOpen={showAuthModal} />}
-      <div className='flex flex-col items-center'>
+      <div className='flex flex-col items-center min-w-1/2'>
         <h4 className="text-xl text-center py-4 p-4">
           Hi <i>audiophile</i>! Welcome to <DeplayerTitle />
         </h4>
         <Image />
       </div>
-      <div className='px-6 md:py-8'>
-        <p className='py-4 text-base-content'>
-          Access to you good ol' music library and enjoy it whenever you need it. <br />
-          To start playing some content follow one of the steps below:
-        </p>
-        { rows.length === 0 && (
-          <div className='mb-6'>
-            <TryDemoButton dispatch={dispatch} />
+      <div className='bg-base-300 mt-4 mx-10 my-10 glass'>
+        <div className='bg-base-200 p-4 px-6 md:py-8'>
+          <p className='py-4 text-base-content'>
+            Access to you good ol' music library and enjoy it whenever you need it. <br />
+            To start playing some content follow one of the steps below:
+          </p>
+          { Object.keys(rows).length === 0 && (
+            <div className='mb-6'>
+              <TryDemoButton dispatch={dispatch} />
+            </div>
+          )}
+          <ul>
+            <li><Link to='/providers' className='text-primary hover:text-primary-focus'>Setup your media providers</Link>, (Subsonic API, mstream or ITunes)</li>
+            <li>
+              <a onClick={() => dispatch({ type: types.SHOW_ADD_MEDIA_MODAL })} className='text-primary hover:text-primary-focus cursor-pointer'>Add new media to your collection</a> Webtorrent, Filesystem, IPFS or youtube-dl-server
+            </li>
+            <li><Link to='/collection' className='text-primary hover:text-primary-focus'>Or go to your collection</Link></li>
+          </ul>
+          <div className='pt-6 flex flex-col items-center md:justify-start'>
+            {credentials && <p className='py-2 text-base-content'>You are authenticated with your passkey</p>}
+            {!credentials && (
+              <>
+                <p className='py-2 text-base-content'>Access social capabilities by authenticating with your passkey</p>
+                <Button long onClick={() => setShowAuthModal(true)}>🔒 Auth</Button>
+              </>
+            )
+            }
           </div>
-        )}
-        <ul>
-          <li><Link to='/providers' className='text-primary hover:text-primary-focus'>Setup your media providers</Link>, (Subsonic API, mstream or ITunes)</li>
-          <li>
-            <a onClick={() => dispatch({ type: types.SHOW_ADD_MEDIA_MODAL })} className='text-primary hover:text-primary-focus cursor-pointer'>Add new media to your collection</a> Webtorrent, Filesystem, IPFS or youtube-dl-server
-          </li>
-          <li><Link to='/collection' className='text-primary hover:text-primary-focus'>Or go to your collection</Link></li>
-        </ul>
-        <div className='pt-6 flex flex-col items-center md:justify-start'>
-          {credentials && <p className='py-2 text-base-content'>You are authenticated with your passkey</p>}
-          {!credentials && (
-            <>
-              <p className='py-2 text-base-content'>Access social capabilities by authenticating with your passkey</p>
-              <Button long onClick={() => setShowAuthModal(true)}>🔒 Auth</Button>
-            </>
-          )
-          }
         </div>
       </div>
     </div>
@@ -99,8 +100,6 @@ const Dashboard = ({
   dispatch
 }: Props) => {
   const MAX_LIST_ITEMS = 25
-  const RandomImage = pickImage()
-  const hasItems = Object.keys(rows).length > 0
 
   const mediaItems = songsByNumberOfPlays.map((songId: string) => {
     return rows[songId]
