e720357c83f01b9aab30ac77b6296f5285ee240a
Genar Trias Ortiz <gtrias@gmail.com>
2025-02-18 10:21:39 +0100

    add missing file

---
+++ src/reducers/queue/types.ts
@@ -0,0 +1,24 @@
+import * as types from '../../constants/ActionTypes'
+import { IMedia } from '../../entities/Media'
+
+export type QueueAction = 
+  | { type: typeof types.ADD_SONGS_TO_QUEUE_BY_ID; trackIds: string[]; replace?: boolean }
+  | { type: typeof types.ADD_SONGS_TO_QUEUE; songs: IMedia[]; replace?: boolean }
+  | { type: typeof types.SET_CURRENT_PLAYING; songId: string }
+  | { type: typeof types.REMOVE_FROM_QUEUE; song?: IMedia; data?: IMedia | IMedia[] }
+  | { type: typeof types.RECEIVE_QUEUE; queue: Partial<State> }
+  | { type: typeof types.ADD_TO_QUEUE; songs: IMedia[] }
+  | { type: typeof types.ADD_TO_QUEUE_NEXT; songs: IMedia[] }
+  | { type: typeof types.CLEAR_QUEUE }
+  | { type: typeof types.SHUFFLE }
+  | { type: typeof types.REPEAT };
+
+export type State = {
+  trackIds: Array<string>;
+  randomTrackIds: Array<string>;
+  currentPlaying: string | null;
+  repeat: boolean;
+  shuffle: boolean;
+  nextSongId: string | null;
+  prevSongId: string | null;
+}; 
