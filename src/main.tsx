cfde96b17fb7f555bda548aeca86ccb58f458de9
Genar Trias Ortiz <gtrias@gmail.com>
2025-04-14 17:37:46 +0200

    react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import "./init"
 
+import { scan } from 'react-scan'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+
+scan({
+  enabled: import.meta.env.DEV,
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
