51838fd36c6edc1f8777282fb59db52fa105c19a
Genar Trias <gtrias@gmail.com>
2026-03-19 00:30:59 +0100

    feat: add BecauseYouListened component + i18n keys

---
+++ src/components/BecauseYouListened/index.tsx
@@ -0,0 +1,42 @@
+import { Translate } from 'react-redux-i18n'
+import HorizontalSlider from '../HorizontalSlider'
+import MediaCover from '../common/AlbumCover'
+import { useRecommendations } from '../../stores/livestore/hooks/useRecommendations'
+
+type Props = {
+  artistId: string
+  artistName: string
+  genres: string[]
+  limit?: number
+}
+
+const BecauseYouListened = ({ artistId, artistName, genres, limit = 15 }: Props) => {
+  const recommendations = useRecommendations(artistId, artistName, genres, limit)
+
+  if (!recommendations.length) return null
+
+  const items = recommendations
+    .filter(item => item && item.id)
+    .map(item => (
+      <MediaCover
+        key={item.id}
+        id={item.id}
+        name={item.title || item.name || 'Unknown'}
+        artistName={item.artistName}
+        cover={item.cover || { thumbnailUrl: '', fullUrl: '' }}
+        type="song"
+      />
+    ))
+
+  if (!items.length) return null
+
+  const title = (
+    <span>
+      <Translate value="titles.becauseYouListenedTo" /> {artistName}
+    </span>
+  )
+
+  return <HorizontalSlider title={title} items={items} />
+}
+
+export default BecauseYouListened
