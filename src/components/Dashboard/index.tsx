eb152f783cfda84c25654b938e79d0c78609d198
Genar Trias <gtrias@gmail.com>
2026-03-19 00:40:32 +0100

    feat: restore welcome message with retro SVG illustrations in dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FunctionComponent, SVGProps } from 'react'
 import { Translate } from 'react-redux-i18n'
 import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
@@ -13,32 +13,83 @@ import Footer from '../Footer'
 import TryDemoButton from '../Buttons/TryDemoButton'
 import DashboardCard from './DashboardCard'
 import DeplayerTitle from '../DeplayerTitle'
+import Auth from '../Auth'
+import Button from '../common/Button'
 import * as types from '../../constants/ActionTypes'
 import { useRecentlyPlayed, useMediaCount } from '../../stores/livestore/hooks'
 
+import RecordPlayerSvg from './record-player.svg?react'
+import CasseteSvg from './cassete.svg?react'
+import WalkmanSvg from './walkman.svg?react'
+import DiscmanSvg from './discman.svg?react'
+import HeadsetSvg from './headset.svg?react'
+import PhonophoneSvg from './phonophone.svg?react'
+
+const IMAGE_COMPONENTS: FunctionComponent<SVGProps<SVGSVGElement>>[] = [
+  RecordPlayerSvg,
+  CasseteSvg,
+  WalkmanSvg,
+  DiscmanSvg,
+  HeadsetSvg,
+  PhonophoneSvg
+]
+
+const pickImage = () => IMAGE_COMPONENTS[Math.floor(Math.random() * IMAGE_COMPONENTS.length)]
+
+const RandomIllustration = () => {
+  const ImageComponent = React.useMemo(() => pickImage(), [])
+  return <ImageComponent className="w-32 h-auto fill-base-content opacity-80" />
+}
+
 const WelcomeCard = () => {
   const dispatch = useDispatch()
+  const [showAuthModal, setShowAuthModal] = React.useState(false)
+  const credentials = localStorage.getItem('credentials')
 
   return (
     <DashboardCard title="Welcome" icon="faMusic" className="col-span-full">
-      <div className="flex flex-col items-center gap-4 py-4">
-        <h4 className="text-xl text-center">
-          <Translate value="dashboard.welcome.title" dangerousHTML /> <DeplayerTitle />
-        </h4>
-        <p className="text-center opacity-70">
-          <Translate value="dashboard.welcome.description" />
-        </p>
-        <div className="flex flex-col sm:flex-row gap-3 items-center">
-          <TryDemoButton />
-          <Link to="/providers" className="btn btn-outline btn-sm">
-            <Translate value="dashboard.welcome.setupProviders" />
-          </Link>
-          <a
-            onClick={() => dispatch({ type: types.SHOW_ADD_MEDIA_MODAL })}
-            className="btn btn-outline btn-sm cursor-pointer"
-          >
-            <Translate value="dashboard.welcome.addMedia" />
-          </a>
+      {showAuthModal && <Auth />}
+      <div className="flex flex-col md:flex-row items-center gap-6 py-2">
+        <RandomIllustration />
+        <div className="flex flex-col gap-3 flex-1">
+          <h4 className="text-xl">
+            <Translate value="dashboard.welcome.title" dangerousHTML /> <DeplayerTitle />
+          </h4>
+          <p className="opacity-70">
+            <Translate value="dashboard.welcome.description" /><br />
+            <Translate value="dashboard.welcome.steps" />
+          </p>
+          <ul className="flex flex-col gap-1">
+            <li>
+              <Link to="/providers" className="text-primary hover:text-primary-focus">
+                <Translate value="dashboard.welcome.setupProviders" />
+              </Link>
+            </li>
+            <li>
+              <a onClick={() => dispatch({ type: types.SHOW_ADD_MEDIA_MODAL })} className="text-primary hover:text-primary-focus cursor-pointer">
+                <Translate value="dashboard.welcome.addMedia" />
+              </a>{' '}
+              <Translate value="dashboard.welcome.addMediaDescription" />
+            </li>
+            <li>
+              <Link to="/collection" className="text-primary hover:text-primary-focus">
+                <Translate value="dashboard.welcome.goToCollection" />
+              </Link>
+            </li>
+          </ul>
+          <div className="flex flex-wrap gap-3 items-center pt-2">
+            <TryDemoButton />
+            {!credentials && (
+              <Button onClick={() => setShowAuthModal(true)}>
+                <Translate value="dashboard.welcome.authButton" />
+              </Button>
+            )}
+            {credentials && (
+              <span className="text-sm opacity-70">
+                <Translate value="dashboard.welcome.authenticated" />
+              </span>
+            )}
+          </div>
         </div>
       </div>
     </DashboardCard>
