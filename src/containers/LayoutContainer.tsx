afa5c17acc247946c22dac9f1e765a211601d2d7
Genar Trias <gtrias@gmail.com>
2026-06-01 12:38:11 +0200

    refactor(ui): migrate LayoutContainer to useUIStore

--- src/containers/LayoutContainer.tsx
+++ src/containers/LayoutContainer.tsx
@@ -1,47 +1,45 @@
-import { connect } from 'react-redux'
 import * as React from 'react'
-import { Dispatch } from 'redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Sidebar from '../components/Sidebar/Sidebar'
 import TopbarContainer from './TopbarContainer'
 import SearchButton from '../components/Buttons/SearchButton'
 import Placeholder from '../components/Player/Placeholder'
 import ReloadPrompt from '../components/ReloadPrompt'
-import type { State as AppState } from '../reducers/app'
 import type { State } from '../reducers'
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import RightPanelContainer from './RightPanelContainer'
 import ButterchurnVisualizer from '../components/ButterchurnVisualizer'
 import PlayerRefService from '../services/PlayerRefService'
-import type { State as PlayerState } from '../reducers/player'
+import { useUIStore } from '../stores/uiStore'
 
 interface LayoutProps {
-  backgroundImage: string,
-  dispatch: Dispatch,
-  app: AppState,
-  player: PlayerState,
   children: React.ReactNode
 }
 
-function Layout(props: LayoutProps) {
+const Layout = ({ children }: LayoutProps) => {
+  const dispatch = useDispatch()
+  const backgroundImage = useUIStore((s) => s.backgroundImage)
+  const mqlMatch = useUIStore((s) => s.mqlMatch)
+  const showVisuals = useUIStore((s) => s.showVisuals)
+  const fullscreen = useSelector((state: State) => state.player.fullscreen)
+
   const getInternalPlayer = () => {
     const playerRef = PlayerRefService.getInstance().getPlayerRef()
     if (!playerRef?.current) return null
     const internalPlayer = playerRef.current.getInternalPlayer()
-    if (internalPlayer instanceof HTMLAudioElement) {
-      return internalPlayer
-    }
+    if (internalPlayer instanceof HTMLAudioElement) return internalPlayer
     return null
   }
 
   const internalPlayer = getInternalPlayer()
 
-  const background = props.backgroundImage && (
+  const background = backgroundImage && (
     <>
       <div className='bg-handler before:bg-base-200/70'></div>
       <div
         className='absolute w-full h-full bg-cover bg-center bg-no-repeat bg-fixed glass'
-        style={{ backgroundImage: `url(${props.backgroundImage})`, filter: 'blur(10px)' }}
+        style={{ backgroundImage: `url(${backgroundImage})`, filter: 'blur(10px)' }}
       />
     </>
   )
@@ -55,35 +53,23 @@ function Layout(props: LayoutProps) {
         <TopbarContainer>
           <SearchButton />
         </TopbarContainer>
-
         <div className='layout-contents z-10'>
-          {props.children}
+          {children}
         </div>
-        <Placeholder mqlMatch={props.app.mqlMatch} />
-
-        </Sidebar>
-        {internalPlayer && props.app.showVisuals && (
-          <ButterchurnVisualizer
-            playerRef={internalPlayer}
-            fullscreen={props.player.fullscreen}
-            width={window.innerWidth}
-            height={window.innerHeight}
-            dispatch={props.dispatch}
-          />
-        )}
+        <Placeholder mqlMatch={mqlMatch} />
+      </Sidebar>
+      {internalPlayer && showVisuals && (
+        <ButterchurnVisualizer
+          playerRef={internalPlayer}
+          fullscreen={fullscreen}
+          width={window.innerWidth}
+          height={window.innerHeight}
+          dispatch={dispatch}
+        />
+      )}
       <RightPanelContainer />
     </>
   )
 }
 
-const connector = connect(
-  (state: State) => {
-    return {
-      backgroundImage: state.app.backgroundImage,
-      app: state.app,
-      player: state.player,
-    }
-  }
-)
-
-export default connector(Layout)
+export default Layout
